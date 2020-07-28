d7faf817794c4e4fa2a4b000ad5399f96f984168
Schmiels <56640760+Schmiels@users.noreply.github.com>
2020-07-28 12:39:27 +0200

    Update WeatherController.java
    - request handling
    - removed todos
    - some logging

--- spring-weather-app/src/main/java/de/fh/albsig/hs88455/spring_weather_app/controller/WeatherController.java
+++ spring-weather-app/src/main/java/de/fh/albsig/hs88455/spring_weather_app/controller/WeatherController.java
@@ -1,25 +1,41 @@
 package de.fh.albsig.hs88455.spring_weather_app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import de.fh.albsig.hs88455.spring_weather_app.models.Weather;
 import de.fh.albsig.hs88455.spring_weather_app.services.WeatherService;
 
 @RestController
 public class WeatherController {
-
-	@GetMapping("/")
-	public String home(@RequestParam(value="cityName", defaultValue="") String cityName) {
+	
+	@PostMapping("/weather")
+	public String weather(
+			@RequestParam(value="cityId", required=false, defaultValue="0") int cityId,
+			@RequestParam(value="cityName", required=false, defaultValue="") String cityName,
+			@RequestParam(value="lat", required=false, defaultValue="0") double lat,
+			@RequestParam(value="lon", required=false, defaultValue="0") double lon,
+			@RequestParam(value="countryCode", required=false, defaultValue="") String countryCode) {
+		Logger logger = LogManager.getLogger(WeatherController.class);
+		
 		WeatherService weatherService = new WeatherService();
-		System.out.println(cityName);
-		Weather weather = weatherService.getWeatherByCityName(cityName);
+		Weather weather = new Weather();
+		
+		
+		if (cityId != 0) {
+			weather = weatherService.getWeatherByCityId(cityId);
+		} else if (!cityName.equals("")) {
+			weather = weatherService.getWeatherByCityName(cityName, countryCode);
+		} else if (lat != 0 && lon != 0) {
+			weather = weatherService.getWeatherByCoords(lat, lon);
+		} else {
+			logger.error("The given input could not be used to request weather data.");
+			return "The given input could not be used to request weather data.";
+		}
 		
 		return weather.toXML();
 	}
-	
-	// TODO: SvenBartos: Eine Methode für Darstellung einer Startseite mit Optionen für Abfrage der Wetterdaten
-	// TODO: SvenBartos: Eine Methode für Darstellung der abgefragten Daten (Objekt als XML)
 }
