3a0a8adb87c886b4c8f14a98a83745a97301cd02
Schmiels <56640760+Schmiels@users.noreply.github.com>
2020-07-28 09:48:01 +0200

    Update Main.java

--- spring-weather-app/src/main/java/de/fh/albsig/hs88455/spring_weather_app/Main.java
+++ spring-weather-app/src/main/java/de/fh/albsig/hs88455/spring_weather_app/Main.java
@@ -2,16 +2,23 @@ package de.fh.albsig.hs88455.spring_weather_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class Main 
-{
-    public static void main( String[] args )
-    {
-    	SpringApplication.run(Main.class, args);
-    }
+public class Main extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Main.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
 }
