e2661a2979857b0aa2348ead51468d8d0ed42fe0
Schmiels <56640760+Schmiels@users.noreply.github.com>
2020-07-28 15:50:11 +0200

    Update WeatherController.java
    checkstyle

--- spring-weather-app/src/main/java/de/fh/albsig/hs88455/controller/WeatherController.java
+++ spring-weather-app/src/main/java/de/fh/albsig/hs88455/controller/WeatherController.java
@@ -1,13 +1,12 @@
 package de.fh.albsig.hs88455.controller;
 
+import de.fh.albsig.hs88455.models.Weather;
+import de.fh.albsig.hs88455.services.WeatherService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import de.fh.albsig.hs88455.models.Weather;
-import de.fh.albsig.hs88455.services.WeatherService;
 
 /**
  * WeatherController.
@@ -18,42 +17,42 @@ import de.fh.albsig.hs88455.services.WeatherService;
 @RestController
 public class WeatherController {
 
-	/**
-	 * REST-Api endpoint.
-	 *
-	 * Retrieve weather data with a POST request
-	 * <p>
-	 * test
-	 *
-	 * @param cityId
-	 * @param cityName
-	 * @param lat
-	 * @param lon
-	 * @param countryCode
-	 * @return String
-	 */
-	@PostMapping("/weather")
-	public String weather(@RequestParam(value = "cityId", required = false, defaultValue = "0") final int cityId,
-			@RequestParam(value = "cityName", required = false, defaultValue = "") final String cityName,
-			@RequestParam(value = "lat", required = false, defaultValue = "0") final double lat,
-			@RequestParam(value = "lon", required = false, defaultValue = "0") final double lon,
-			@RequestParam(value = "countryCode", required = false, defaultValue = "") final String countryCode) {
-		Logger logger = LogManager.getLogger(WeatherController.class);
+  /**
+   * REST-Api endpoint.
+   *
+   * <p>Retrieve weather data with a POST request
+   *
+   * @param cityId cityId
+   * @param cityName cityName
+   * @param lat latitude
+   * @param lon longitude
+   * @param countryCode countryCode
+   * @return String
+   */
+  @PostMapping("/weather")
+  public String weather(
+      @RequestParam(value = "cityId", required = false, defaultValue = "0") final int cityId,
+      @RequestParam(value = "cityName", required = false, defaultValue = "") final String cityName,
+      @RequestParam(value = "lat", required = false, defaultValue = "0") final double lat,
+      @RequestParam(value = "lon", required = false, defaultValue = "0") final double lon,
+      @RequestParam(value = "countryCode", required = false,
+          defaultValue = "") final String countryCode) {
+    Logger logger = LogManager.getLogger(WeatherController.class);
 
-		WeatherService weatherService = new WeatherService();
-		Weather weather = new Weather();
+    WeatherService weatherService = new WeatherService();
+    Weather weather = new Weather();
 
-		if (cityId != 0) {
-			weather = weatherService.getWeatherByCityId(cityId);
-		} else if (!cityName.equals("")) {
-			weather = weatherService.getWeatherByCityName(cityName, countryCode);
-		} else if (lat != 0 && lon != 0) {
-			weather = weatherService.getWeatherByCoords(lat, lon);
-		} else {
-			logger.error("The given input could not be used to request weather " + "data.");
-			return "The given input could not be used to request weather " + "data.";
-		}
+    if (cityId != 0) {
+      weather = weatherService.getWeatherByCityId(cityId);
+    } else if (!cityName.equals("")) {
+      weather = weatherService.getWeatherByCityName(cityName, countryCode);
+    } else if (lat != 0 && lon != 0) {
+      weather = weatherService.getWeatherByCoords(lat, lon);
+    } else {
+      logger.error("The given input could not be used to request weather " + "data.");
+      return "The given input could not be used to request weather " + "data.";
+    }
 
-		return weather.toXML();
-	}
+    return weather.toXml();
+  }
 }
