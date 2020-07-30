07b3e38debe8e858f7a92f84e6591c99f355a839
Schmiels <56640760+Schmiels@users.noreply.github.com>
2020-07-30 12:55:57 +0200

    Update WeatherService.java
    - exception handling
    - common-lang3
    - implemented interface
    - new method for zipcode

--- spring-weather-app/src/main/java/de/fh/albsig/hs88455/services/WeatherService.java
+++ spring-weather-app/src/main/java/de/fh/albsig/hs88455/services/WeatherService.java
@@ -1,9 +1,11 @@
 package de.fh.albsig.hs88455.services;
 
 import de.fh.albsig.hs88455.exceptions.CustomOpenWeatherException;
+import de.fh.albsig.hs88455.interfaces.IWeatherService;
 import de.fh.albsig.hs88455.models.Weather;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
@@ -12,7 +14,9 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.stereotype.Service;
 
 // import de.fh.albsig.hs88455.spring_weather_app.models.Weather;
 
@@ -21,7 +25,8 @@ import org.json.JSONObject;
  *
  * @author svenb
  */
-public class WeatherService {
+@Service
+public class WeatherService implements IWeatherService {
 
   private final String apiKey = "0faf94f3aa783dbf3030947b5c36cf41";
 
@@ -50,7 +55,8 @@ public class WeatherService {
    * @param types GET parameters' names
    * @return String
    */
-  private String buildUrl(String[] parameters, String[] types) {
+  @Override
+  public String buildUrl(String[] parameters, String[] types) {
     URIBuilder builder = new URIBuilder();
     builder.setScheme("http");
     builder.setHost("api.openweathermap.org");
@@ -75,37 +81,61 @@ public class WeatherService {
    * @return weather object
    * @throws CustomOpenWeatherException Exception being thrown on openweather-api calls
    */
-  private Weather requestWeatherData(String[] parameters, String[] types)
+  @Override
+  public Weather requestWeatherData(String[] parameters, String[] types)
       throws CustomOpenWeatherException {
-    String respBody;
+    String respBody = null;
 
     CloseableHttpClient httpClient = HttpClients.createDefault();
 
     try {
-      HttpGet get = new HttpGet(this.buildUrl(parameters, types));
+      String url = this.buildUrl(parameters, types);
+      HttpGet get = new HttpGet(url);
 
       CloseableHttpResponse resp = httpClient.execute(get);
+      try {
+        int statusCode = resp.getStatusLine().getStatusCode();
+        if (statusCode != 200) {
+          String errorMsg = "Error while performing API request: ";
+          logger.error(errorMsg + "{}", statusCode);
+          throw new CustomOpenWeatherException(errorMsg + statusCode, null);
+        }
 
-      int statusCode = resp.getStatusLine().getStatusCode();
-      if (statusCode != 200) {
-        String errorMsg = "Error while performing API request: ";
-        logger.error(errorMsg + "{}", statusCode);
-        throw new CustomOpenWeatherException(errorMsg + statusCode, null);
+        respBody = EntityUtils.toString(resp.getEntity(), StandardCharsets.UTF_8);
+      } finally {
+        resp.close();
       }
-
-      respBody = EntityUtils.toString(resp.getEntity(), StandardCharsets.UTF_8);
-
-      resp.close();
-
-      JSONObject jsonObj = new JSONObject(respBody);
-
-      WeatherParser weatherParser = new WeatherParser();
-      Weather weather = weatherParser.parseFromJson(jsonObj);
-
-      return weather;
-    } catch (IOException e) {
+      if (StringUtils.isEmpty(respBody)) {
+        logger.error("API call returned empty answer at {}", url);
+        throw new CustomOpenWeatherException("API call returned empty answer at " + url, null);
+      } else {
+        JSONObject jsonObj = new JSONObject(respBody);
+        WeatherParser weatherParser = new WeatherParser();
+        Weather weather = weatherParser.parseFromJson(jsonObj);
+        return weather;
+      }
+    } catch (IOException | JSONException e) {
       logger.error(e.getMessage());
       throw new CustomOpenWeatherException("An Error accured while performing the API request", e);
+    }
+  }
+
+  /**
+   * Get weather information by cityName.
+   *
+   * @param cityName cityName
+   * @return weather object
+   * @throws CustomOpenWeatherException Exception being thrown on openweather-api calls
+   */
+  @Override
+  public Weather getWeatherByCityName(String cityName) throws CustomOpenWeatherException {
+    String[] parameters = {cityName};
+    String[] types = {"q"};
+
+    try {
+      return this.requestWeatherData(parameters, types);
+    } catch (CustomOpenWeatherException e) {
+      throw new CustomOpenWeatherException(e.getMessage(), e);
     }
   }
 
@@ -117,7 +147,8 @@ public class WeatherService {
    * @return weather object
    * @throws CustomOpenWeatherException Exception being thrown on openweather-api calls
    */
-  public Weather getWeatherByCityName(String cityName, String countryCode)
+  @Override
+  public Weather getWeatherByCityNameAndCountryCode(String cityName, String countryCode)
       throws CustomOpenWeatherException {
     String[] parameters = {cityName.concat(",").concat(countryCode)};
     String[] types = {"q"};
@@ -136,6 +167,7 @@ public class WeatherService {
    * @return weather object
    * @throws CustomOpenWeatherException Exception being thrown on openweather-api calls
    */
+  @Override
   public Weather getWeatherByCityId(int cityId) throws CustomOpenWeatherException {
     String[] parameters = {Integer.toString(cityId)};
     String[] types = {"id"};
@@ -155,9 +187,24 @@ public class WeatherService {
    * @return weather object
    * @throws CustomOpenWeatherException Exception being thrown on openweather-api calls
    */
+  @Override
   public Weather getWeatherByCoords(double lat, double lon) throws CustomOpenWeatherException {
     String[] parameters = {Double.toString(lat), Double.toString(lon)};
     String[] types = {"lat", "lon"};
+
+    try {
+      return this.requestWeatherData(parameters, types);
+    } catch (CustomOpenWeatherException e) {
+      throw new CustomOpenWeatherException(e.getMessage(), e);
+    }
+  }
+
+
+  @Override
+  public Weather getWeatherByZipCode(int zipCode, String countryCode)
+      throws CustomOpenWeatherException {
+    String[] parameters = {Integer.toString(zipCode).concat(",").concat(countryCode)};
+    String[] types = {"zip"};
 
     try {
       return this.requestWeatherData(parameters, types);
